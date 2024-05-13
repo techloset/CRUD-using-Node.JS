521b1a0347be3df97d0ec2e04b3b479e405f19e9
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-13 12:15:11 +0500

    feat:Applied some conditions

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,7 +1,11 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 function Add() {
+  const URL = "http://localhost:8000";
+
   const initialState = {
     title: "",
     location: "",
@@ -17,7 +21,39 @@ function Add() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    let { title, description, location } = state;
+    if ((!title, !description, !location)) {
+      return toast.error("Please fill all the fields");
+    }
+
+    if (title.length < 3) {
+      return toast.error("Please enter a title");
+    }
+    if (location.length < 3) {
+      return toast.error("Please enter a location");
+    }
+    if (description.length < 3) {
+      return toast.error("Please enter a description");
+    }
+
+    let todo = {
+      title,
+      description,
+      location,
+      status: "active",
+      dateCreated: new Date().getTime(),
+    };
+
     console.log(state);
+    axios
+      .post(`${URL}/addTodo`, todo)
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
   };
 
   return (
