f4271eb3fd9150cd012a98404564c526fbb5c0c3
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-13 11:44:34 +0500

    feat:Make form + installed axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.scss";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Add from "./pages/Add";
 
 function App() {
-  return <></>;
+  return <Add />;
 }
 
 export default App;
---
+++ src/pages/Add.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+
+function Add() {
+  const initialState = {
+    title: "",
+    location: "",
+    description: "",
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((s) => ({ ...s, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
+
+  return (
+    <div className="py-5">
+      <div className="container">
+        <div className="row mb-4">
+          <div className="col">
+            <div className="h1 text-center mb-0 ">Add Todo</div>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="card p-3 p-md-4 mx-auto " style={{ maxWidth: 500 }}>
+            <div className="row">
+              <div className="col-12-col-md-6-mb-3">
+                <input
+                  type="text"
+                  placeholder="Title"
+                  name="title"
+                  className="form-control mb-3"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-12-col-md-6-mb-3">
+                <input
+                  type="text"
+                  placeholder="Location"
+                  name="location"
+                  className="form-control mb-3"
+                  onChange={handleChange}
+                />
+                <div className="col mb-3">
+                  <textarea
+                    name="description"
+                    placeholder="Description"
+                    className="form-control"
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
+                <div className="col-12 col-md-6 offset-md-3 ">
+                  <button className="btn btn-primary w-100">Add Todo</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Add;
